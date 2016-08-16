5e8c2953c445ec022c7135b616aa73f04d4388b7
howellzx804 <chollingsworth@fyrsoft.com>
2016-08-16 12:27:12 -0500

    Created Test-Cory.ps1 in AzureAutomation.

---
+++ Runbooks/Test-Cory.ps1
@@ -0,0 +1,19 @@
+workflow Test-Cory
+{
+    
+    param ( 
+        [object]$WebhookData
+    ) 
+
+        # Collect properties of WebhookData
+        $WebhookName    =   $WebhookData.WebhookName
+        $WebhookHeaders =   $WebhookData.RequestHeader
+        $WebhookBody    =   $WebhookData.RequestBody
+
+		$WebhookName
+		$WebhookHeaders
+		$WebhookBody = $WebhookBody | ConvertFrom-Json
+		$WebhookBody | gm
+		$WebhookBody | fl
+		
+}
